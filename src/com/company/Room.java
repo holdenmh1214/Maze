39b9bb5484305f18c989bb480a164173758c4ea2
holdenhughes <holdenmh121489@gmail.com>
2015-12-01 12:02:23 -0500

    Initial commit

---
+++ src/com/company/Room.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by holdenhughes on 12/1/15.
+ */
+public class Room {
+    int row;
+    int col;
+    boolean wasVisited = false;
+    boolean hasBottom = true;
+    boolean hasRight = true;
+
+
+    public Room(int row, int col) {
+    }
+}
