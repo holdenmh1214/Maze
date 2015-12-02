2b11b6e3473c0f3a8d8a6799eb06e530562b255a
holdenhughes <holdenmh121489@gmail.com>
2015-12-01 21:21:12 -0500

    Include a start and end

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,11 @@ public class Main {
 
     static final int SIZE = 10;
 
-    static ArrayList<ArrayList<Room>> createRooms(){
-        ArrayList<ArrayList<Room>> rooms = new ArrayList<>();
-
-        for (int row=0; row < SIZE; row++){
-            ArrayList<Room> roomRow = new ArrayList<>();
-            for (int col =0; col < SIZE; col++){
+    static ArrayList<ArrayList<Room>> createRooms() {
+        ArrayList<ArrayList<Room>> rooms = new ArrayList();
+        for (int row = 0; row < SIZE; row++) {
+            ArrayList<Room> roomRow = new ArrayList();
+            for (int col = 0; col < SIZE; col++) {
                 roomRow.add(new Room(row, col));
             }
             rooms.add(roomRow);
@@ -21,23 +20,23 @@ public class Main {
         return rooms;
     }
 
-    static ArrayList<Room> possibleNeighbors(ArrayList<ArrayList<Room>> rooms, int row, int col){
-        ArrayList<Room> neighbors = new ArrayList<>();
-      if (row > 0) neighbors.add(rooms.get(row-1).get(col)); //going up
-      if (row < SIZE-1) neighbors.add(rooms.get(row+1).get(col)); //going down
-      if (col > 0) neighbors.add(rooms.get(row).get(col-1)); //going left
-      if (col < SIZE-1) neighbors.add(rooms.get(row).get(col+1)); //going right
+    static ArrayList<Room> possibleNeighbors(ArrayList<ArrayList<Room>> rooms, int row, int col) {
+        ArrayList<Room> neighbors = new ArrayList();
+        if (row > 0) neighbors.add(rooms.get(row-1).get(col));
+        if (row < SIZE-1) neighbors.add(rooms.get(row+1).get(col));
+        if (col > 0) neighbors.add(rooms.get(row).get(col-1));
+        if (col < SIZE-1) neighbors.add(rooms.get(row).get(col+1));
         return neighbors;
     }
 
-    static Room randomNeighbor (ArrayList<ArrayList<Room>> rooms, int row, int col){
+    static Room randomNeighbor(ArrayList<ArrayList<Room>> rooms, int row, int col) {
         ArrayList<Room> neighbors = possibleNeighbors(rooms, row, col);
         neighbors = neighbors.stream()
-                .filter(room ->{
+                .filter(room -> {
                     return !room.wasVisited;
                 })
                 .collect(Collectors.toCollection(ArrayList<Room>::new));
-        if (neighbors.size() >0){
+        if (neighbors.size() > 0) {
             Random r = new Random();
             int index = r.nextInt(neighbors.size());
             return neighbors.get(index);
@@ -45,54 +44,74 @@ public class Main {
         return null;
     }
 
-    static void tearDownWall(Room oldRoom, Room newRoom){
-        //going up
-        if (newRoom.row < oldRoom.row){
+    static void tearDownWall(Room oldRoom, Room newRoom) {
+        // going up
+        if (newRoom.row < oldRoom.row) {
             newRoom.hasBottom = false;
         }
-        //going down
-        else if (newRoom.row > oldRoom.row){
+        // going down
+        else if (newRoom.row > oldRoom.row) {
             oldRoom.hasBottom = false;
         }
-        //going left
-        else if (newRoom.col < oldRoom.col){
+        // going left
+        else if (newRoom.col < oldRoom.col) {
             newRoom.hasRight = false;
         }
-        else if (newRoom.col > oldRoom.col){
+        // going right
+        else if (newRoom.col > oldRoom.col) {
             oldRoom.hasRight = false;
         }
     }
 
-    static boolean createMaze (ArrayList<ArrayList<Room>> rooms, Room room){
+    static boolean createMaze(ArrayList<ArrayList<Room>> rooms, Room room) {
         room.wasVisited = true;
         Room nextRoom = randomNeighbor(rooms, room.row, room.col);
-        if (nextRoom == null){
+        if (nextRoom == null) {
+            boolean temp = false;
+            for (ArrayList<Room> roomList : rooms){
+                for (Room tempRoom : roomList){
+                    if (tempRoom.isEnd){
+                        temp = true;
+                    }
+                }
+            }
+            if(!temp){
+                room.isEnd = true;
+            }
             return false;
         }
+
         tearDownWall(room, nextRoom);
 
-        while (createMaze(rooms, nextRoom)){
+        while (createMaze(rooms, nextRoom)) {
 
         }
+
         return true;
     }
 
     public static void main(String[] args) {
         ArrayList<ArrayList<Room>> rooms = createRooms();
         createMaze(rooms, rooms.get(0).get(0));
-        for(ArrayList<Room> roomRow : rooms){
+        rooms.get(0).get(0).isStart = true;
+        for (ArrayList<Room> roomRow : rooms) {
             System.out.print(" _");
         }
         System.out.println();
         for (ArrayList<Room> roomRow : rooms) {
             System.out.print("|");
-            for (Room room: roomRow){
+            for (Room room : roomRow) {
                 String s1 = room.hasBottom ? "_" : " ";
+                if (room.isStart){
+                    s1 ="o";
+                }
+                if (room.isEnd){
+                    s1 = "x";
+                }
                 String s2 = room.hasRight ? "|" : " ";
                 System.out.print(s1 + s2);
             }
             System.out.println();
         }
-
     }
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -9,8 +9,11 @@ public class Room {
     boolean wasVisited = false;
     boolean hasBottom = true;
     boolean hasRight = true;
-
+    boolean isStart = false;
+    boolean isEnd = false;
 
     public Room(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 }
